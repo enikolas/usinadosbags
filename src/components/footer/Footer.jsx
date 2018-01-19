294200eb421f4c68b15f888f18536d4313ea94bd
Everton Nikolas de Oliveira <enikolas.oliveira@gmail.com>
2018-01-19 13:23:10 -0200

    Fix the Footer font-size and make it responsive ready.

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,13 +4,13 @@ import './Footer.css'
 const Footer = () => (
   <footer className='Footer'>
     <div className='left'>
-      <h3>Usina dos Bags</h3>
+      <h5>Usina dos Bags</h5>
       <p>SP +55 11 2087-1384</p>
       <p>SP +55 11 98740-0139</p>
       <p>vendas@usinadosbags.com.br</p>
     </div>
     <div className='right'>
-      <h3>Copyright © 2009-2018</h3>
+      <h5>Copyright © 2009-2018</h5>
       <p>Todos os direitos reservados</p>
     </div>
   </footer>
