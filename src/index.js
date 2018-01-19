4dad6dbd98d7cd1e6b140d19eb9b31ac96d64e72
Everton Nikolas de Oliveira <enikolas.oliveira@gmail.com>
2018-01-19 13:27:07 -0200

    Remove react-snapshot as Google is already ready for Single Page Applications

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react'
-import { render } from 'react-snapshot'
+import ReactDOM from 'react-dom'
 import './index.css'
 import App from './components/App'
 import registerServiceWorker from './registerServiceWorker'
 
-render(<App />, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById('root'))
 registerServiceWorker()
