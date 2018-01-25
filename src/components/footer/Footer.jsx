be72bc55caa0c10c8632e53cdbb250c05986e30c
Everton Nikolas de Oliveira <enikolas.oliveira@gmail.com>
2018-01-25 16:44:32 -0200

    Add more requested phone numbers

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,8 +5,10 @@ const Footer = () => (
   <footer className='Footer'>
     <div className='left'>
       <h5>Usina dos Bags</h5>
+      <p>SP +55 11 2087-1383</p>
       <p>SP +55 11 2087-1384</p>
-      <p>SP +55 11 98740-0139</p>
+      <p>SP +55 11 4962-1384</p>
+      <p>SP +55 11 9 8740-0139</p>
       <p>vendas@usinadosbags.com.br</p>
     </div>
     <div className='right'>
