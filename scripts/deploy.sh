a19fb41c3fdc0b46dadb55052051079ecae216e0
Everton Nikolas de Oliveira <enikolas.oliveira@gmail.com>
2018-01-28 21:56:09 -0200

    Improve settings
    - Improve the manigest.json file to work properly with react-router
    - Improve the firebase.json cache of resources to 24h
    - Add again PUBLIC_URL to travis env variable
    - Remove unused commands from deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,10 +3,4 @@ set -e
 
 cd build
 
-echo $CI
-
-export PUBLIC_URL=http://www.usinadosbags.com.br/
-
 find . -type f -exec curl --ftp-create-dirs -T {} -u $FTP_USER:$FTP_PASSWORD ftp://$FTP_URL/{} \;
-
-cd -
