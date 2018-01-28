18b9a2c6dbf571ec33d8b930ba150b7cf5316981
Everton Nikolas de Oliveira <enikolas.oliveira@gmail.com>
2018-01-28 20:15:22 -0200

    Trying to fix the deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,12 @@
+#!/bin/bash
+set -e
+
 cd build
+
 echo $CI
+
 export PUBLIC_URL=http://www.usinadosbags.com.br/
+
 find . -type f -exec curl --ftp-create-dirs -T {} -u $FTP_USER:$FTP_PASSWORD ftp://$FTP_URL/{} \;
+
+cd -
