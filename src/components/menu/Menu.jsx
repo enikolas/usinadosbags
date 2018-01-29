f61032ab6a1396080b44f1d0bf40aa8c149289f8
Everton Nikolas de Oliveira <enikolas.oliveira@gmail.com>
2018-01-28 23:42:10 -0200

    Fix #11

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -9,7 +9,13 @@ class Menu extends Component {
   constructor (props) {
     super(props)
 
+    this._onClick = this._onClick.bind(this)
     this.state = { isOpen: false }
+  }
+
+  _onClick () {
+    const { isOpen } = this.state
+    this.setState({ isOpen: !isOpen })
   }
 
   render () {
@@ -17,42 +23,18 @@ class Menu extends Component {
     const { isOpen } = this.state
 
     return (
-      <nav className='Menu' onClick={() => { isOpen && this.setState({ isOpen: false }) }}>
+      <nav className='Menu' onClick={this._onClick}>
         <BurguerMenu
           isOpen={isOpen}
           onClick={() => { this.setState({ isOpen: !isOpen }) }} />
         <div className={`MainMenu ${isOpen ? 'open' : ''}`}>
           <ul>
-            <MenuItem
-              label='home'
-              to='/'
-              path={pathname}
-              icon='⬤' />
-            <MenuItem
-              label='sobre nós'
-              to='/sobrenos'
-              path={pathname}
-              icon='⬤' />
-            <MenuItem
-              label='produtos'
-              to='/produtos'
-              path={pathname}
-              icon='⬤' />
-            <MenuItem
-              label='fotos'
-              to='/fotos'
-              path={pathname}
-              icon='⬤' />
-            <MenuItem
-              label='serviços'
-              to='/servicos'
-              path={pathname}
-              icon='⬤' />
-            <MenuItem
-              label='contato'
-              to='/contato'
-              path={pathname}
-              icon='⬤' />
+            <MenuItem label='home' to='/' path={pathname} icon='⬤' />
+            <MenuItem label='sobre nós' to='/sobrenos' path={pathname} icon='⬤' />
+            <MenuItem label='produtos' to='/produtos' path={pathname} icon='⬤' />
+            <MenuItem label='fotos' to='/fotos' path={pathname} icon='⬤' />
+            <MenuItem label='serviços' to='/servicos' path={pathname} icon='⬤' />
+            <MenuItem label='contato' to='/contato' path={pathname} icon='⬤' />
           </ul>
         </div>
       </nav>
