6ddc5d0631366fd1cf9f13967cf950cae21f564d
Guimels <guimel@usinadosbags.com.br>
2018-08-13 11:40:51 -0300

    incluir telefone
    tel santos
    e inclusao de cidade em cada numero

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -7,9 +7,10 @@ const Footer = () => (
 	<footer className="Footer">
 		<div className="left">
 			<h5>Usina dos Bags</h5>
-			<p>SP +55 11 2087-1383</p>
-			<p>SP +55 11 2087-1384</p>
-			<p>SP +55 11 4962-1384</p>
+			<p>SP +55 11 2087-1383 Guarulhos</p>
+			<p>SP +55 11 2087-1384 Guarulhos</p>
+			<p>SP +55 11 4962-1384 Guarulhos</p>
+			<p>SP +55 13 3323-0313 Santos</p>
 			<p>SP +55 11 9 8740-0139</p>
 			<p>vendas@usinadosbags.com.br</p>
 		</div>
