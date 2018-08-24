8da7e40cf695fc7778f7f0992f988fb68fad3bd6
Guimels <guimel@usinadosbags.com.br>
2018-08-24 10:12:34 -0300

    adicionado numero Santos e numero celular

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -7,11 +7,11 @@ const Footer = () => (
 	<footer className="Footer">
 		<div className="left">
 			<h5>Usina dos Bags</h5>
-			<p>SP +55 11 2087-1383 Guarulhos</p>
-			<p>SP +55 11 2087-1384 Guarulhos</p>
-			<p>SP +55 11 4962-1384 Guarulhos</p>
-			<p>SP +55 13 3323-0313 Santos</p>
-			<p>SP +55 11 9 8740-0139</p>
+			<p>SP +55 13 3323-0313</p>
+			<p>SP +55 11 2087-1384</p>
+			<br />
+			<p>SP +55 11 9 7240-0140</p>
+			<p>SP +55 11 9 9339-1012</p>
 			<p>vendas@usinadosbags.com.br</p>
 		</div>
 		<div className="right">
